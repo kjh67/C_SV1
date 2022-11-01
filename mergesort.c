fd4960d1320d15cdc0bd3f91860dafd45b52b10f
K Hawkins <kjh67@cam.ac.uk]>
2022-11-01 17:18:55 +0000

    Comment cleanup

--- mergesort.c
+++ mergesort.c
@@ -12,47 +12,43 @@ void display_arr(int* start, int* end) {
 }
 
 
-// END POINTER IS INCLUSIVE; FOR 1 ELEMENT ARR, START=END
 void merge(int *start, int *end) {
-    // POINTERS TO THE FIRST AND LAST IN AN ARRAY - ASSUME CONTIGUOUS MEM ALLOCATION    
     if (start != end) {
-        // need to split further
-        int *h = start + (end-start)/2; // not sure whether this will auto-round or error - we'll find out!
 
-        // get sorted subarrays
+        // sort subarrays
+        int *h = start + (end-start)/2;
         merge(start, h);
         merge(h + 1, end);
 
-        // copy first subarray into extra memory
+        // copy front subarray into extra memory
         int bufsize = ((long int) h - (long int) start) / sizeof(int) + 1;
         int *b = malloc(sizeof(int) * (bufsize));
         for (int i=0; i<(bufsize); i++) {
             b[i] = start[i];
         }
 
-        // merge into the front of the original array
-        // will only need to finish copying the first half back in afterwards!
-        // second half will already be in the right place, as they stayed put
+        // copy buffer pointer so we can free the memory later
         int* buff_pointer = b;
-        int* write_pointer = start;
-        h++;
+
+        h++; // h now points to front of the second subarray
         while (bufsize > 0) {
             // compare the values stored at each pointer
-            // if we add from front half, increment b and decrement remaining
-            // if we add from back half, increment h
-            // DON'T CHECK BEYOND THE END
+            // if we merge from front half, increment b and decrement remaining
+            // if we merge from back half, increment h
+            // DON'T GO BEYOND END OF THE BUFFER
 
             if ((h <= end) && (*h < *b)) {
                 // take from back
-                *write_pointer = *h;
-                h++; write_pointer++;
+                *start = *h;
+                h++; start++;
             }
             else {
                 // take from front
-                *write_pointer = *b;
-                b++; write_pointer++; bufsize--;
+                *start = *b;
+                b++; start++; bufsize--;
             }
         }
+
         free(buff_pointer);
     }
 }
